c859c558e07a03ccd302d7dbf7426f43b11d2a57
Murillo Oliveira <murillo1047@gmail.com>
2024-12-01 14:46:29 -0300

    feat: checkoutt microsservice

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/basket")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var basket = await _repository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart cart)
+        {
+            return Ok(await _repository.UpdateBasket(cart));
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteBasket(string username)
+        {
+            await _repository.DeleteBasket(username);
+            return Ok();
+        }
+    }
+}
---
+++ Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redis;
+        public BasketRepository(IDistributedCache redis)
+        {
+            _redis = redis ?? throw new ArgumentNullException(nameof(redis));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redis.GetStringAsync(username);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonSerializer.Deserialize<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redis.SetStringAsync(basket.Username, JsonSerializer.Serialize(basket));
+            return await GetBasket(basket.Username);
+        }
+        public async Task DeleteBasket(string username)
+        {
+            await _redis.RemoveAsync(username);
+        }
+    }
+}
